59b57e7a85505f0ec9aee2bcffbc818725db2449
Patryk Kwiatkowski <patseb@gmail.com>
2012-02-22 15:01:26 +0100

    add edge_create function

--- scgl_edge.c
+++ scgl_edge.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "simclist.h"
+#include "scgl_edge.h"
+#include "scgl_pair.h"
+#include "scgl_vertex.h"
 
+edge_t*
+edge_create(char *id, vertex_t *from, vertex_t *to, int is_directed, double weight, pair_t **attr, unsigned int attr_n) {
+	edge_t *e;
+	unsigned int i;
+
+	e = (edge_t*) malloc(sizeof(edge_t));
+	e->id = (char*) malloc(strlen(id)+1);
+	strcpy(e->id, id);
+
+	e->from = from;
+	e->to = to;
+	if (from != NULL)
+		list_append(from->out, e);
+	if (to != NULL)
+		list_append(to->in, e);
+
+	e->is_directed = is_directed;
+	e->weight = weight;
+	list_init(e->attributes);
+	list_attributes_seeker(e->attributes, pair_seeker);
+	list_attributes_comparator(e->attributes, pair_comparator);
+
+	for (i=0; i<attr_n; ++i)
+		list_append(e->attributes, (void*)attr[i]);
+
+	return e;
+}
--- scgl_edge.h
+++ scgl_edge.h
@@ -7,6 +7,7 @@ extern "C" {
 
 #include "simclist.h"
 #include "scgl_vertex.h"
+#include "scgl_pair.h"
 
 typedef struct {
 	/* edge identyficator */
@@ -21,6 +22,20 @@ typedef struct {
 	/* user-purpose list of edge attributes */
 	list_t *attributes;
 } scgl_edge_t;
+
+/**
+ * create edge object, allocate and initialize memory
+ *
+ * @param id	unique edge identifier
+ * @param from	edge source (vertex object)
+ * @param from	edge destination (vertex object)
+ * @param is_directed 1 means that edge is directed, 0 opposite
+ * @param weight	edge weight
+ * @param attr	table of pointer to pair objects, user-purpose attributes
+ * @param attr_n	lenght of attributes table
+ * @return	pointer to edge object for success, NULL for failure
+ */
+edge_t* edge_create(char *id, vertex_t *from, vertex_t *to, int is_directed, double weight, pair_t **attr, unsigned int attr_n);
 
 #ifdef __cplusplus
 }
