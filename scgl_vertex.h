6c4a5f6847ceca94ae96f1db8348ef52a8681f24
Patryk Kwiatkowski <patseb@gmail.com>
2012-02-22 13:45:21 +0100

    add vertex object representation

--- scgl_vertex.h
+++ scgl_vertex.h
@@ -5,6 +5,18 @@
 extern "C" {
 #endif
 
+#include "simclist.h"
+
+/* vertex object */
+typedef struct {
+	/* vertex identyficator */
+	char *id;
+	/* list of edges which are directed to vertex */
+	list_t *in;
+	/* list of edges which are directed out of vertex */
+	list_t *out;
+} scgl_vertex_t;
+
 #ifdef __cplusplus
 }
 #endif
