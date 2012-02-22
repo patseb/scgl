0740815308cf70468a0b44fc3c4b0fc4b7d428aa
Patryk Kwiatkowski <patseb@gmail.com>
2012-02-22 13:58:41 +0100

    add edge object representation

--- scgl_edge.h
+++ scgl_edge.h
@@ -5,6 +5,23 @@
 extern "C" {
 #endif
 
+#include "simclist.h"
+#include "scgl_vertex.h"
+
+typedef struct {
+	/* edge identyficator */
+	char *id;
+	/* 0 - edge is undirected, edge is 1 directed */
+	int is_directed;
+	double weight;
+	/* edge source vertex */
+	scgl_vertex_t *from;
+	/* edge destination vertex */
+	scgl_vertex_t *to;
+	/* user-purpose list of edge attributes */
+	list_t *attributes;
+} scgl_edge_t;
+
 #ifdef __cplusplus
 }
 #endif
