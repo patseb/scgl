21ebf06d13d252aab58d0fa194bf0f84887411a8
Patryk Kwiatkowski <patseb@gmail.com>
2012-02-22 14:05:51 +0100

    add vertex_create function

--- scgl_vertex.c
+++ scgl_vertex.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "simclist.h"
+#include "scgl_edge.h"
+#include "scgl_vertex.h"
 
+vertex_t*
+vertex_create(char *id, edge_t **in, unsigned int in_n, edge_t **out, unsigned int out_n) {
+	vertex_t *v;
+	unsigned int i;
+
+	v = (vertex_t*) malloc(sizeof(vertex_t));
+
+	v->id = (char*) malloc(strlen(id)+1);
+	strcpy(v->id, id);
+
+	list_init(v->in);
+	list_init(v->out);
+
+	for (i=0; i<in_n; ++i)
+		list_append(v->in, (void*)in[i]);
+
+	for (i=0; i<out_n; ++i)
+		list_append(v->out, (void*)out[i]);
+
+	return v;
+}
--- scgl_vertex.h
+++ scgl_vertex.h
@@ -6,6 +6,7 @@ extern "C" {
 #endif
 
 #include "simclist.h"
+#include "scgl_edge.h"
 
 /* vertex object */
 typedef struct {
@@ -16,6 +17,18 @@ typedef struct {
 	/* list of edges which are directed out of vertex */
 	list_t *out;
 } scgl_vertex_t;
+
+/**
+ * create vertex object, allocate and initialize memory
+ *
+ * @param id	unique vertex identyficator
+ * @param in	table of pointers to edge_t objects which are directed to vertex, can be NULL
+ * @param in_n	in table length, should be 0 if 'in' is NULL
+ * @param out	table of pointers to edge_t objects which are directed out of vertex, can be NULL
+ * @param out_n	out table length, should be 0 if 'out' is NULL
+ * @return	pointer to vertex object for success, NULL for failure
+ */
+vertex_t* vertex_create(char *id, edge_t **in, unsigned int in_n, edge_t **out, unsigned int out_n)
 
 #ifdef __cplusplus
 }
