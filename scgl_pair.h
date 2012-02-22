206380747b9140fde696e84873e0430695f60384
Patryk Kwiatkowski <patseb@gmail.com>
2012-02-22 14:50:16 +0100

    missing ; after function declaration fix

--- scgl_pair.h
+++ scgl_pair.h
@@ -17,18 +17,18 @@ typedef struct {
  * @param value	value indexed by key
  * @return	pointer to pair object for success, NULL for failure
  */
-pair_t* scgl_pair_create(const char *key, void *value)
+pair_t* scgl_pair_create(const char *key, void *value);
 
 /**
  * free memory occupied by pair object
  *
  * @param pair	pair object
  */
-void pair_destroy(pair_t *pair)
+void pair_destroy(pair_t *pair);
 
 /* internal functions section */
-int pair_seeker(const void *elem, const void *key)
-int pair_comparator(const void *a, const void *b)
+int pair_seeker(const void *elem, const void *key);
+int pair_comparator(const void *a, const void *b);
 
 #ifdef __cplusplus
 }
