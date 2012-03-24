01a2db96aa9eed565429a6e2ec1eb67f1726225d
Patryk Kwiatkowski <patseb@gmail.com>
2012-03-24 21:52:59 +0100

    init attr_free_fun attribute when creating edge

--- scgl_edge.c
+++ scgl_edge.c
@@ -29,9 +29,9 @@ scgl_edge_create(char *id, scgl_vertex_t *from, scgl_vertex_t *to, int is_direct
 	list_init(e->attributes);
 	list_attributes_seeker(e->attributes, scgl_pair_seeker);
 	list_attributes_comparator(e->attributes, scgl_pair_comparator);
-
 	for (i=0; i<attr_n; ++i)
 		list_append(e->attributes, (void*)attr[i]);
+	e->attr_free_fun = NULL;
 
 	return e;
 }
