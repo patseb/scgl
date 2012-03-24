972059210d1043971190e3a6928dba636609bb25
Patryk Kwiatkowski <patseb@gmail.com>
2012-03-24 21:54:41 +0100

    check if free_function is not NULL

--- scgl_pair.c
+++ scgl_pair.c
@@ -18,7 +18,8 @@ scgl_pair_create(const char *key, void *value) {
 void
 scgl_pair_destroy(scgl_pair_t *pair, attr_free_function fun) {
 	if (pair != NULL) {
-		(*fun)(pair->key, pair->value);
+		if (fun != NULL)
+			(*fun)(pair->key, pair->value);
 		free(pair->key);
 		free(pair);
 		pair = NULL;
