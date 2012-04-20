fda24c89224900568f03b2ba543363df398c824b
Patryk Kwiatkowski <patseb@gmail.com>
2012-04-20 15:25:10 +0200

    clean up typedefs and header files

---
+++ scgl_def.h
@@ -0,0 +1,17 @@
+#ifndef SCGL_DEF_H
+#define SCGL_DEF_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct scgl_pair scgl_pair_t;
+typedef struct scgl_edge scgl_edge_t;
+typedef struct scgl_vertex scgl_vertex_t;
+typedef struct scgl_graph scgl_graph_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
