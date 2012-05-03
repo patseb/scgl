965f7813efce72283d18d76faf845d656c4feef4
Patryk Kwiatkowski <patseb@gmail.com>
2012-05-03 14:06:39 +0200

    extend dijkstra description

--- include/scgl_algorithms.h
+++ include/scgl_algorithms.h
@@ -5,20 +5,26 @@
 extern "C" {
 #endif
 
+#include "scgl_edge.h"
 #include "scgl_def.h"
 
 /**
  * function calculates shortest patch from source to every vertex in graph
  * function implements dijkstra algorithm with heap based priority queue
+ * results will be stored in p and d tables.
+ * p will contain predecessor of every vertex in graph
+ * d will contain distance from source to every vertex in graph
+ * p and d will be indexed: 0 ... count(vertexes), 0 means vertex added first to graph, 1 means second added vertex, etc.
+ * You can get reference to vertex by scgl_graph_get_vertex_at()
  *
  * @param graph reference to graph object
  * @param scr   reference to source vertex
  * @praram p    reference to predecessors table, function will allocate enought memory (vertexes count)
  * @param d     reference to distance table, function will allocate enought memory (vertexex count)
- *<F7>
  *
+ * @see scgl_graph_get_vertex_at()
  */
-void scgl_dijkstra(const scgl_graph_t *graph, scgl_vertex_t *src, unsigned int **p, cost_type **d);
+void scgl_dijkstra(const scgl_graph_t *graph, scgl_vertex_t *src, unsigned int **p, cost_type_t **d);
 
 #ifdef __cplusplus
 }
