c4a3f550694bb5514c0f1d7776c78135b0874bbf
Patryk Kwiatkowski <patseb@gmail.com>
2012-05-04 20:41:53 +0200

    remove unnecessary typedefs

--- include/scgl.h
+++ include/scgl.h
@@ -5,11 +5,6 @@
 extern "C" {
 #endif
 
-typedef struct scgl_attr scgl_attr_t;
-typedef struct scgl_edge scgl_edge_t;
-typedef struct scgl_vertex scgl_vertex_t;
-typedef struct scgl_graph scgl_graph_t;
-
 #include "scgl_attr.h"
 #include "scgl_edge.h"
 #include "scgl_vertex.h"
