a8f91e2a8642463fed09d79e18c8f2c8eeb4c18b
Patryk Kwiatkowski <patseb@gmail.com>
2012-05-16 18:25:03 +0200

    scgl dejagnu unit tests

--- tests/scgl.test/tests.c
+++ tests/scgl.test/tests.c
@@ -9,7 +9,7 @@ void edge_attr_free(char *key, void *value, void **data) {
 
 void edge_attr_dump(char *key, void *value, void **fp) {
 	if (fp != NULL && *fp != NULL)
-		fprintf((FILE*)*fp, "\t\t%s : %s \n", key, (char*)value);
+		fprintf((FILE*)*fp, "%s %s ", key, (char*)value);
 }
 
 void edge_attr_copy(char *key, void *value, void **data) {
@@ -28,6 +28,112 @@ void edge_attr_copy(char *key, void *value, void **data) {
 void edge_foreach(scgl_edge_t *edge, void **data) {
 	if (edge != NULL)
 		printf(cost_fmt " ", edge->cost);
+}
+
+void dijkstra_test(unsigned int undirected) {
+	scgl_graph_t *g1;
+	scgl_vertex_t **v;
+	scgl_edge_t **e;
+	unsigned int *p = NULL;
+	cost_type *d = NULL;
+	unsigned int i, n = 13, m = 22;
+
+	g1 = scgl_graph_create(NULL, NULL, 0, NULL, 0);
+
+	v = (scgl_vertex_t**) malloc(sizeof(scgl_vertex_t*) * n);
+	for(i=0; i<n; ++i) {
+		v[i] = scgl_vertex_create(NULL, NULL, 0, NULL, 0);
+		scgl_graph_add_vertex(g1, v[i]);
+	}
+
+	e = (scgl_edge_t**) malloc(sizeof(scgl_edge_t*) * m);
+	for(i=0; i<m; ++i) {
+		e[i] = scgl_edge_create(NULL, NULL, undirected, 0, NULL, 0);
+		scgl_graph_add_edge(g1, e[i]);
+	}
+
+	scgl_edge_add_vertex( e[0], v[0],  0);
+	scgl_edge_add_vertex( e[0], v[1],  1);
+	scgl_edge_add_vertex( e[1], v[0],  0);
+	scgl_edge_add_vertex( e[1], v[2],  1);
+	scgl_edge_add_vertex( e[2], v[2],  0);
+	scgl_edge_add_vertex( e[2], v[1],  1);
+	scgl_edge_add_vertex( e[3], v[1],  0);
+	scgl_edge_add_vertex( e[3], v[6],  1);
+	scgl_edge_add_vertex( e[4], v[1],  0);
+	scgl_edge_add_vertex( e[4], v[5],  1);
+	scgl_edge_add_vertex( e[5], v[5],  0);
+	scgl_edge_add_vertex( e[5], v[6],  1);
+	scgl_edge_add_vertex( e[6], v[6],  0);
+	scgl_edge_add_vertex( e[6], v[7],  1);
+	scgl_edge_add_vertex( e[7], v[7],  0);
+	scgl_edge_add_vertex( e[7], v[8],  1);
+	scgl_edge_add_vertex( e[8], v[8],  0);
+	scgl_edge_add_vertex( e[8], v[6],  1);
+	scgl_edge_add_vertex( e[9], v[5],  0);
+	scgl_edge_add_vertex( e[9], v[8],  1);
+	scgl_edge_add_vertex(e[10], v[5],  0);
+	scgl_edge_add_vertex(e[10], v[4],  1);
+	scgl_edge_add_vertex(e[11], v[8],  0);
+	scgl_edge_add_vertex(e[11], v[9],  1);
+	scgl_edge_add_vertex(e[12], v[4],  0);
+	scgl_edge_add_vertex(e[12], v[9],  1);
+	scgl_edge_add_vertex(e[13], v[9],  0);
+	scgl_edge_add_vertex(e[13], v[10], 1);
+	scgl_edge_add_vertex(e[14], v[12], 0);
+	scgl_edge_add_vertex(e[14], v[9],  1);
+	scgl_edge_add_vertex(e[15], v[9],  0);
+	scgl_edge_add_vertex(e[15], v[11], 1);
+	scgl_edge_add_vertex(e[16], v[11], 0);
+	scgl_edge_add_vertex(e[16], v[3],  1);
+	scgl_edge_add_vertex(e[17], v[10], 0);
+	scgl_edge_add_vertex(e[17], v[12], 1);
+	scgl_edge_add_vertex(e[18], v[10], 0);
+	scgl_edge_add_vertex(e[18], v[3],  1);
+	scgl_edge_add_vertex(e[19], v[4],  0);
+	scgl_edge_add_vertex(e[19], v[3],  1);
+	scgl_edge_add_vertex(e[20], v[11], 0);
+	scgl_edge_add_vertex(e[20], v[2],  1);
+	scgl_edge_add_vertex(e[21], v[3],  0);
+	scgl_edge_add_vertex(e[21], v[12], 1);
+	scgl_edge_set_cost(e[0],   1);
+	scgl_edge_set_cost(e[1],   1);
+	scgl_edge_set_cost(e[2],   1);
+	scgl_edge_set_cost(e[3],   4);
+	scgl_edge_set_cost(e[4],  14);
+	scgl_edge_set_cost(e[5],  10);
+	scgl_edge_set_cost(e[6],   8);
+	scgl_edge_set_cost(e[7],   9);
+	scgl_edge_set_cost(e[8],  19);
+	scgl_edge_set_cost(e[9],  12);
+	scgl_edge_set_cost(e[10], 15);
+	scgl_edge_set_cost(e[11],  9);
+	scgl_edge_set_cost(e[12],  9);
+	scgl_edge_set_cost(e[13],  6);
+	scgl_edge_set_cost(e[14],  5);
+	scgl_edge_set_cost(e[15], 16);
+	scgl_edge_set_cost(e[16], 11);
+	scgl_edge_set_cost(e[17], 13);
+	scgl_edge_set_cost(e[18], 18);
+	scgl_edge_set_cost(e[19],  2);
+	scgl_edge_set_cost(e[20],  7);
+	scgl_edge_set_cost(e[21], 17);
+
+	scgl_dijkstra(g1, v[0], &p, &d);
+	for(i=0; i<n; ++i)
+		printf("%d, ", p[i]);
+	for(i=0; i<n; ++i)
+		printf(cost_fmt ", ", d[i]);
+	printf("\n");
+
+	scgl_graph_destroy(&g1, edge_attr_free);
+
+	free(p);
+	free(d);
+
+	free(v);
+	free(e);
+
 }
 
 int main(int argc, char **argv) {
@@ -121,12 +227,11 @@ int main(int argc, char **argv) {
 					scgl_vertex_foreach_edge(v1, 1, edge_foreach, NULL);
 					scgl_vertex_foreach_edge(v1, 0, edge_foreach, NULL);
 					printf("\n");
+					scgl_edge_destroy(&e1, NULL);
+					scgl_edge_destroy(&e2, NULL);
+					scgl_vertex_destroy(&v1);
 				} break;
 
-				case '6':
-					break;
-				case '7':
-					break;
 				default:
 					return -1;
 			} break;
@@ -159,6 +264,20 @@ int main(int argc, char **argv) {
 					printf("%s \n", scgl_attr_get_key(a1));
 					scgl_attr_destroy(&a1, edge_attr_free);
 				} break;
+
+				case '3': {
+					buf1 = (char*) malloc(5);
+					buf2 = (char*) malloc(5);
+					sprintf(buf1, "val1");
+					sprintf(buf2, "val2");
+					a1 = scgl_attr_create(NULL, buf1);
+					if (a1 == NULL)
+						return -1;
+					printf("%s ", (char*)scgl_attr_get_value(a1));
+					scgl_attr_set_value(a1, buf2, edge_attr_free);
+					printf("%s \n", (char*)scgl_attr_get_value(a1));
+					scgl_attr_destroy(&a1, edge_attr_free);
+				} break;
 			} break;
 
 		case 'e':
@@ -178,6 +297,8 @@ int main(int argc, char **argv) {
 					printf("%s %s " cost_fmt " " cost_fmt,
 					       e1->from->id, e1->to->id, e1->cost, e1->sibling->cost);
 					scgl_edge_destroy(&e1, NULL);
+					scgl_vertex_destroy(&v1);
+					scgl_vertex_destroy(&v2);
 					printf(" %d\n", (int)e1);
 				} break;
 
@@ -225,47 +346,228 @@ int main(int argc, char **argv) {
 				} break;
 
 				case '4': {
+					buf1 = (char*) malloc(5);
+					buf2 = (char*) malloc(5);
+					sprintf(buf1, "key1");
+					sprintf(buf2, "val1");
 					e1 = scgl_edge_create(NULL, NULL, 0, 1, NULL, 0);
-					
+					if (e1 == NULL)
+						return -1;
+					scgl_edge_add_attribute(e1, buf1, buf2);
+					printf("%s %s %s ",
+					       buf1,
+					       buf2,
+					       (char*)scgl_edge_get_attribute_value(e1, "key1"));
+					scgl_edge_del_attribute(e1, "key1", edge_attr_free);
+					printf("%d\n",
+					       (int)scgl_edge_get_attribute_value(e1, "key1"));
+					scgl_edge_destroy(&e1, edge_attr_free);
 				} break;
-				case '5':
-					break;
-				case '6':
-					break;
-				case '7':
-					break;
+
+				case '5': {
+					buf1 = (char*) malloc(2);
+					sprintf(buf1, "1");
+					e1 = scgl_edge_create(NULL, NULL, 0, 1, NULL, 0);
+					if (e1 == NULL)
+						return -1;
+					scgl_edge_add_attribute(e1, buf1, NULL);
+					printf("%d ", scgl_edge_get_attributes_count(e1));
+					scgl_edge_del_attribute(e1, "1", edge_attr_free);
+					printf("%d\n", scgl_edge_get_attributes_count(e1));
+					scgl_edge_destroy(&e1, edge_attr_free);
+				} break;
+
+				case '6': {
+					buf1 = (char*) malloc(4);
+					buf2 = (char*) malloc(4);
+					sprintf(buf1, "key");
+					sprintf(buf2, "val");
+					e1 = scgl_edge_create(NULL, NULL, 0, 1, NULL, 0);
+					if (e1 == NULL)
+						return -1;
+					scgl_edge_add_attribute(e1, buf1, buf2);
+					scgl_edge_foreach_attribute(e1, edge_attr_dump, stdout);
+					scgl_edge_destroy(&e1, edge_attr_free);
+				} break;
+
+				case '7': {
+					e1 = scgl_edge_create(NULL, NULL, 0, 123, NULL, 0);
+					v1 = scgl_vertex_create(NULL, NULL, 0, NULL, 0);
+					v2 = scgl_vertex_create(NULL, NULL, 0, NULL, 0);
+					if (e1 == NULL || v1 == NULL || v2 == NULL)
+						return -1;
+					scgl_edge_add_vertex(e1, v1, 0);
+					printf("%d " cost_fmt " %d ",
+					       (int)e1->sibling,
+					       e1->cost,
+					       scgl_vertex_get_edges_out_count(v1));
+					scgl_edge_set_undirected(e1, 1);
+					printf("%d ", scgl_edge_get_undirected(e1));
+					printf("%d " cost_fmt " %d\n",
+					       (int)e1->sibling,
+					       e1->sibling->cost,
+					       scgl_vertex_get_edges_in_count(v1));
+					scgl_edge_destroy(&e1, NULL);
+					scgl_vertex_destroy(&v1);
+					scgl_vertex_destroy(&v2);
+				} break;
+
 				default:
 					return -1;
 			} break;
 
 		case 'g':
-			printf("g \n");
 			switch (argv[2][0]) {
-				case '1':
-					break;
-				case '2':
-					break;
-				case '3':
-					break;
-				case '4':
-					break;
-				case '5':
-					break;
-				case '6':
-					break;
-				case '7':
-					break;
+				case '1': {
+					buf1 = (char*) malloc(3);
+					sprintf(buf1, "G1");
+					g1 = scgl_graph_create(buf1, NULL, 0, NULL, 0);
+					if (g1 == NULL)
+						return -1;
+					printf("%s ", g1->id);
+					scgl_graph_destroy(&g1, NULL);
+					printf("%d\n", (int)g1);
+				} break;
+
+				case '2': {
+					buf1 = (char*) malloc(3);
+					sprintf(buf1, "G1");
+					g1 = scgl_graph_create(buf1, NULL, 0, NULL, 0);
+					if (g1 == NULL)
+						return -1;
+					printf("%s ", scgl_graph_get_id(g1));
+					buf1 = (char*) malloc(3);
+					sprintf(buf1, "G2");
+					scgl_graph_set_id(g1, buf1);
+					printf("%s \n", scgl_graph_get_id(g1));
+					scgl_graph_destroy(&g1, NULL);
+				} break;
+
+				case '3': {
+					buf1 = (char*) malloc(3);
+					buf2 = (char*) malloc(3);
+					sprintf(buf1, "V1");
+					sprintf(buf2, "V2");
+					g1 = scgl_graph_create(NULL, NULL, 0, NULL, 0);
+					v1 = scgl_vertex_create(buf1, NULL, 0, NULL, 0);
+					v2 = scgl_vertex_create(buf2, NULL, 0, NULL, 0);
+					if (g1 == NULL || v1 == NULL || v2 == NULL)
+						return -1;
+					scgl_graph_add_vertex(g1, v1);
+					scgl_graph_add_vertex(g1, v2);
+					printf("%d %s %s ",
+					       list_count(&g1->vertexes),
+					       scgl_graph_get_vertex(g1, "V2")->id,
+					       scgl_graph_get_vertex_at(g1, 0)->id);
+					scgl_graph_del_vertex(g1, &v1, NULL);
+					printf("%d %d %s \n",
+					       list_count(&g1->vertexes),
+					       (int)scgl_vertex_get_id(scgl_graph_get_vertex(g1, "V1")),
+					       scgl_graph_get_vertex_at(g1, 0)->id);
+
+					scgl_graph_destroy(&g1, NULL);
+				} break;
+
+				case '4': {
+					g1 = scgl_graph_create(NULL, NULL, 0, NULL, 0);
+					e1 = scgl_edge_create(NULL, NULL, 0, 123, NULL, 0);
+					e2 = scgl_edge_create(NULL, NULL, 0, 321, NULL, 0);
+					if (g1 == NULL || e1 == NULL || e2 == NULL)
+						return -1;
+					scgl_graph_add_edge(g1, e1);
+					scgl_graph_add_edge(g1, e2);
+					printf("%d " cost_fmt " " cost_fmt " ",
+					       list_count(&g1->edges),
+					       scgl_graph_get_edge_at(g1, 0)->cost,
+					       scgl_graph_get_edge_at(g1, 1)->cost);
+					scgl_graph_del_edge(g1, &e1, NULL);
+					printf("%d " cost_fmt " %d\n",
+					       list_count(&g1->edges),
+					       scgl_graph_get_edge_at(g1, 0)->cost,
+					       (int)scgl_graph_get_edge_at(g1, 1));
+
+					scgl_graph_destroy(&g1, NULL);
+				} break;
+
+				case '5': {
+					g1 = scgl_graph_create(NULL, NULL, 0, NULL, 0);
+					v1 = scgl_vertex_create(NULL, NULL, 0, NULL, 0);
+					v2 = scgl_vertex_create(NULL, NULL, 0, NULL, 0);
+					e1 = scgl_edge_create(NULL, NULL, 0, 0, NULL, 0);
+					e2 = scgl_edge_create(NULL, NULL, 0, 0, NULL, 0);
+					if (g1 == NULL || v1 == NULL || v2 == NULL || e1 == NULL || e2 == NULL)
+						return -1;
+					printf("%d %d ",
+					       scgl_graph_get_vertexes_count(g1),
+					       scgl_graph_get_edges_count(g1));
+					
+					scgl_graph_add_edge(g1, e1);
+					scgl_graph_add_edge(g1, e2);
+					scgl_graph_add_vertex(g1, v1);
+					scgl_graph_add_vertex(g1, v2);
+
+					printf("%d %d\n",
+					       scgl_graph_get_vertexes_count(g1),
+					       scgl_graph_get_edges_count(g1));
+
+					scgl_graph_destroy(&g1, NULL);
+				} break;
+
+				case '6': {
+					buf1 = (char*) malloc(3);
+					buf2 = (char*) malloc(3);
+					sprintf(buf1, "V1");
+					sprintf(buf2, "V2");
+					g1 = scgl_graph_create(NULL, NULL, 0, NULL, 0);
+					v1 = scgl_vertex_create(buf1, NULL, 0, NULL, 0);
+					v2 = scgl_vertex_create(buf2, NULL, 0, NULL, 0);
+					e1 = scgl_edge_create(NULL, NULL, 0, 123, NULL, 0);
+					e2 = scgl_edge_create(NULL, NULL, 0, 321, NULL, 0);
+					if (g1 == NULL || v1 == NULL || v2 == NULL || e1 == NULL || e2 == NULL)
+						return -1;
+
+					buf1 = (char*) malloc(4);
+					buf2 = (char*) malloc(4);
+					sprintf(buf1, "key");
+					sprintf(buf2, "val");
+					scgl_edge_add_attribute(e1, buf1, buf2);
+					scgl_edge_add_vertex(e1, v1, 0);
+					scgl_edge_add_vertex(e1, v2, 1);
+					scgl_graph_add_edge(g1, e1);
+					scgl_graph_add_edge(g1, e2);
+					scgl_graph_add_vertex(g1, v1);
+					scgl_graph_add_vertex(g1, v2);
+
+					g2 = scgl_graph_copy(g1, edge_attr_copy);
+					printf("%d %d %d %d %d %s %s %d %d\n",
+					      (g1 == g2),
+					      (scgl_graph_get_vertexes_count(g1) == scgl_graph_get_vertexes_count(g2)),
+					      (scgl_graph_get_edges_count(g1) == scgl_graph_get_edges_count(g2)),
+					      (scgl_graph_get_vertex_at(g1, 0) == scgl_graph_get_vertex_at(g2, 0)),
+					      (scgl_graph_get_edge_at(g1, 1)->cost == scgl_graph_get_edge_at(g2, 1)->cost),
+					      scgl_graph_get_vertex_at(g1, 1)->id,
+					      scgl_graph_get_vertex_at(g2, 1)->id,
+					      (scgl_graph_get_edge_at(g1, 0) == scgl_graph_get_edge_at(g2, 0)),
+					      (scgl_vertex_get_edge_out_at(scgl_graph_get_vertex_at(g1, 0), 0)->cost == scgl_vertex_get_edge_out_at(scgl_graph_get_vertex_at(g2, 0), 0)->cost));
+
+					scgl_graph_destroy(&g1, edge_attr_free);
+					scgl_graph_destroy(&g2, edge_attr_free);
+				} break;
+
 				default:
 					return -1;
 			} break;
 
 		case 'd':
-			printf("a \n");
 			switch (argv[2][0]) {
-				case '1':
-					break;
-				case '2':
-					break;
+				case '1': {
+					dijkstra_test(0);
+				} break;
+
+				case '2': {
+					dijkstra_test(1);
+				} break;
+
 				default:
 					return -1;
 			} break;
@@ -274,149 +576,5 @@ int main(int argc, char **argv) {
 			return -1;
 	}
 
-/*	scgl_graph_t *g1;//, *g2;
-	scgl_vertex_t **v;
-		//free(buf11);
-	scgl_edge_t **e;
-	unsigned int i, n = 13, m = 22;
-	char *buf1;
-
-	buf1 = (char*) malloc(3);
-	sprintf(buf1, "G1");
-	g1 = scgl_graph_create(buf1, NULL, 0, NULL, 0);
-
-	v = (scgl_vertex_t**) malloc(sizeof(scgl_vertex_t*) * n);
-	for(i=0; i<n; ++i) {
-		buf1 = (char*) malloc(4);
-		sprintf(buf1, "%d", i);
-		v[i] = scgl_vertex_create(buf1, NULL, 0, NULL, 0);
-		scgl_graph_add_vertex(g1, v[i]);
-	}
-
-	e = (scgl_edge_t**) malloc(sizeof(scgl_edge_t*) * m);
-	for(i=0; i<m; ++i) {
-		buf1 = (char*) malloc(6);
-		sprintf(buf1, "E-%d", i);
-		e[i] = scgl_edge_create(NULL, NULL, 1, 0, NULL, 0);
-		//free(buf1);
-		char *buf11 = (char*) malloc(10);
-		sprintf(buf11, "%d", i);
-		scgl_edge_add_attribute(e[i], buf1, buf11);
-		scgl_graph_add_edge(g1, e[i]);
-	}
-
-	//for(i=0; i<n-1; ++i) {
-		scgl_edge_set_vertex(e[0], v[0], 0);
-		scgl_edge_set_vertex(e[0], v[1], 1);
-		scgl_edge_set_vertex(e[1], v[0], 0);
-		scgl_edge_set_vertex(e[1], v[2], 1);
-		scgl_edge_set_vertex(e[2], v[2], 0);
-		scgl_edge_set_vertex(e[2], v[1], 1);
-		scgl_edge_set_vertex(e[3], v[1], 0);
-		scgl_edge_set_vertex(e[3], v[6], 1);
-		scgl_edge_set_vertex(e[4], v[1], 0);
-		scgl_edge_set_vertex(e[4], v[5], 1);
-		scgl_edge_set_vertex(e[5], v[5], 0);
-		scgl_edge_set_vertex(e[5], v[6], 1);
-		scgl_edge_set_vertex(e[6], v[6], 0);
-		scgl_edge_set_vertex(e[6], v[7], 1);
-		scgl_edge_set_vertex(e[7], v[7], 0);
-		scgl_edge_set_vertex(e[7], v[8], 1);
-		scgl_edge_set_vertex(e[8], v[8], 0);
-		scgl_edge_set_vertex(e[8], v[6], 1);
-		scgl_edge_set_vertex(e[9], v[5], 0);
-		scgl_edge_set_vertex(e[9], v[8], 1);
-		scgl_edge_set_vertex(e[10], v[5], 0);
-		scgl_edge_set_vertex(e[10], v[4], 1);
-		scgl_edge_set_vertex(e[11], v[8], 0);
-		scgl_edge_set_vertex(e[11], v[9], 1);
-		scgl_edge_set_vertex(e[12], v[4], 0);
-		scgl_edge_set_vertex(e[12], v[9], 1);
-		scgl_edge_set_vertex(e[13], v[9], 0);
-		scgl_edge_set_vertex(e[13], v[10], 1);
-		scgl_edge_set_vertex(e[14], v[12], 0);
-		scgl_edge_set_vertex(e[14], v[9], 1);
-		scgl_edge_set_vertex(e[15], v[9], 0);
-		scgl_edge_set_vertex(e[15], v[11], 1);
-		scgl_edge_set_vertex(e[16], v[11], 0);
-		scgl_edge_set_vertex(e[16], v[3], 1);
-		scgl_edge_set_vertex(e[17], v[10], 0);
-		scgl_edge_set_vertex(e[17], v[12], 1);
-		scgl_edge_set_vertex(e[18], v[10], 0);
-		scgl_edge_set_vertex(e[18], v[3], 1);
-		scgl_edge_set_vertex(e[19], v[4], 0);
-		scgl_edge_set_vertex(e[19], v[3], 1);
-		scgl_edge_set_vertex(e[20], v[11], 0);
-		scgl_edge_set_vertex(e[20], v[2], 1);
-		scgl_edge_set_vertex(e[21], v[3], 0);
-		scgl_edge_set_vertex(e[21], v[12], 1);
-		scgl_edge_set_cost(e[0], 1);
-		scgl_edge_set_cost(e[1], 1);
-		scgl_edge_set_cost(e[2], 1);
-		scgl_edge_set_cost(e[3], 4);
-		scgl_edge_set_cost(e[4], 14);
-		scgl_edge_set_cost(e[5], 10);
-		scgl_edge_set_cost(e[6], 8);
-		scgl_edge_set_cost(e[7], 9);
-		scgl_edge_set_cost(e[8], 19);
-		scgl_edge_set_cost(e[9], 12);
-		scgl_edge_set_cost(e[10], 15);
-		scgl_edge_set_cost(e[11], 9);
-		scgl_edge_set_cost(e[12], 9);
-		scgl_edge_set_cost(e[13], 6);
-		scgl_edge_set_cost(e[14], 5);
-		scgl_edge_set_cost(e[15], 16);
-		scgl_edge_set_cost(e[16], 11);
-		scgl_edge_set_cost(e[17], 13);
-		scgl_edge_set_cost(e[18], 18);
-		scgl_edge_set_cost(e[19], 2);
-		scgl_edge_set_cost(e[20], 7);
-		scgl_edge_set_cost(e[21], 17);
-	//}*/
-/*
-	scgl_edge_destroy(&e[0]);
-	scgl_edge_set_undirected(e[1], 1);
-	//scgl_edge_set_undirected(e[1], 0);
-	scgl_vertex_del_edge(v[1], e[1]);
-	scgl_vertex_add_edge(v[1], e[1], 0);
-	scgl_vertex_add_edge(v[0], e[1], 0);
-	scgl_graph_del_vertex(g1, &v[1]);
-	scgl_vertex_destroy(&v[0]);
-	scgl_graph_del_edge(g1, &e[3]); 
-
-
-	scgl_graph_dump(g1, stdout, edge_attr_dump);
-
-	unsigned int *p = NULL;
-	cost_type *d = NULL;
-	scgl_dijkstra(g1, v[0], &p, &d);
-	printf("pred = ");
-	for(i=0; i<n; ++i)
-		printf("%d, ", p[i]);
-	printf("\ndist = ");
-	for(i=0; i<n; ++i)
-		printf(cost_fmt ", ", d[i]);
-	printf("\n");
-	free(p);
-	free(d);
-
-	scgl_graph_t *g2 = scgl_graph_copy(g1, edge_attr_copy);
-	scgl_graph_dump(g2, stdout, edge_attr_dump);
-
-	unnecessary
-//	for(i=0; i<m; ++i)
-//		scgl_edge_destroy(&e[i], edge_attr_free);
-	unnecessary
-//	for(i=0; i<n; ++i)
-//		scgl_vertex_destroy(&v[i]);
-
-	scgl_graph_destroy(&g1, edge_attr_free);
-	scgl_graph_destroy(&g2, edge_attr_free);
-
-	free(v);
-	free(e);
-
-	//printf ("%d \n ", (sizeof(scgl_vertex_t)*1000) + (sizeof(scgl_edge_t)*1000));
-*/
 	return 0;
 }
