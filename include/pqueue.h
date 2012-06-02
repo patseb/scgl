49b6094bbc7c8a8054fd966bdb31846c8aeeecc3
Patryk Kwiatkowski <patseb@gmail.com>
2012-06-02 14:48:58 +0200

    rewrite pqueue

--- include/pqueue.h
+++ include/pqueue.h
@@ -1,53 +1,82 @@
-#ifndef __PQUEUE__H__
-#define __PQUEUE__H__
+#ifndef PQUEUE_H
+#define PQUEUE_H
 
 /**
-  * Debugging macro .
-*
-* Checks for a NULL pointer, and prints the error message, source file and
-* line via 'stderr' .
-* If the check fails the program exits with error code (-1) .
-*/
-#define NP_CHECK(ptr) \
-		{ \
-			if (NULL == (ptr)) { \
-				fprintf(stderr, "%s:%d NULL POINTER: %s n", \
-						__FILE__, __LINE__, #ptr); \
-				exit(-1); \
-			} \
-		} \
-
-#define DEBUG(msg) fprintf(stderr, "%s:%d %s", __FILE__, __LINE__, (msg))
+ * function which will compare data/priority in queue
+ *
+ * @param data1 reference for data
+ * @param data2 reference for data
+ * @return 0 if datas are the same, negative if data1 has smaller priority, positive if data1 has greater priority
+ *
+ */
+typedef int (*queue_cmp)(const void *data1, const void *data2);
 
 /**
-  * Priority Queue Structure
-  */
-typedef struct PQueue_s {
-	/* The actual size of heap at a certain time */
+ * function which will replace data1 with data2 and free data1 memory
+ *
+ * @param data1 reference for old data
+ * @param data2 reference for new data
+ */
+typedef void (*queue_swp)(void **data1, void **data2);
+
+/* priority queue object */
+struct pqueue {
+	/* actual size of heap */
 	size_t size;
-	/* The amount of allocated memory for the heap */
+	/* amount of allocated memory */
 	size_t capacity;
-	/* An array of (void*), the actual max-heap */
+	/* an array of (void*), the actual max-heap */
 	void **data;
-	/* A pointer to a comparator function, used to prioritize elements */
-	int (*cmp)(const void *d1, const void *d2);
-} PQueue;
+	/* a pointer to a comparator function */
+	queue_cmp cmp;
+};
 
-/** Allocates memory for a new Priority Queue .
-Needs a pointer to a comparator function, thus establishing priorities .
-*/
-PQueue *pqueue_new(int (*cmp)(const void *d1, const void *d2),
-                              size_t capacity);
+typedef struct pqueue pqueue_t;
 
-/** De-allocates memory for a given Priority Queue */
-void pqueue_delete(PQueue *q);
+/**
+ * function allocates memory for new queue
+ *
+ * @param zrobić to po mojemu
+ * @return reference to new priority queue, or NULL for failure
+ *
+ */
+pqueue_t* pqueue_create(int (*cmp)(const void *d1, const void *d2), size_t capacity);
 
-/** Add an element inside the Priority Queue */
-void pqueue_enqueue(PQueue *q, const void *data);
+/**
+ * free memory occupied by queue and set reference to NULL
+ *
+ * @param queue reference to queue object
+ */
+void pqueue_destroy(pqueue_t **queue);
 
-/** Removes the element with the greatest priority from within the Queue */
-void *pqueue_dequeue(PQueue *q);
+/**
+ * add an element inside into the queue
+ *
+ * @param quque reference to queue object
+ * @param data  reference to user data to be added
+ */
+void pqueue_enqueue(pqueue_t *queue, const void *data);
 
-void pqueue_change_data(PQueue *q, int (*cmp)(const void *d1, const void *d2), void *old_data, void (*swp)(void **d1, void **d2), void *new_data);
+/**
+ * removes the element with the greatest priority from queue
+ *
+ * @param queue reference to queue object
+ * @return user data wich has greatest priority
+ */
+void* pqueue_dequeue(pqueue_t *queue);
+
+/**
+ * replace data pointed by compare function into new one
+ *
+ * @param queue reference to queue object
+ * @param old_data  reference to old data which is inside the queue
+ * @param new_data  reference to new data which will be inserted
+ * @param cmp   reference to function which will compare data
+ * @param swp   reference to function which will replace data
+ *
+ * @see queue_cmp()
+ * @see queue_swp()
+ */
+void pqueue_replace_data(pqueue_t *queue, void *old_data, void *new_data, queue_cmp cmp, queue_swp swp);
 
 #endif
